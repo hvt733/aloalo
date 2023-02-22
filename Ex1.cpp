87c643297b3efce0e45f085b05fccd35f313f605
hvt733 <115387551+hvt733@users.noreply.github.com>
2023-02-22 15:16:10 +0700

    ex1

--- Ex1.cpp
+++ Ex1.cpp
@@ -1,1 +1,27 @@
-.
+#include <stdio.h>
+#include <math.h>
+ 
+double ln(double x)
+{
+    x = x - 1;
+    if (-1 <= x && x <= 1)
+    {
+        double t = x, _ln = x; long k = 1;
+        do
+            {
+                t = -t*x*k/(k+1);
+                _ln = _ln + t;
+                k++;
+            }  while (_ln != _ln +t);
+        return _ln;
+    }
+    else return ln(1 + 1/x) - ln(1/x);
+}
+ 
+void main()
+{   
+    //thay x tùy ý//
+    double x = 5;
+    printf("\nHam cua ta viet, ln(%lf)= %.13lf\n",x,ln(x));
+    printf("\nHam cua math.h, log(%lf)= %.13lf\n",x,log(x));
+}
