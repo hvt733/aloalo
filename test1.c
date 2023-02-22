10ef77b805afa79af05495a8e6750c4b216daaea
Nguyen Van Thang <thangnv.bi12-403@st.usth.edu.vn>
2023-02-22 15:14:08 +0700

    m check cho t cai nay di thanh

--- test1.c
+++ test1.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <math.h>
+double calculate(int n){
+    double value;
+    double sum=1/2;
+    for(int i=1 ; i<n ; i++){
+        value=pow(-1,i+1)/((i)*(i+1));
+        sum=sum+value;
+    }
+    return sum;
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("%f", calculate(n));
+}
